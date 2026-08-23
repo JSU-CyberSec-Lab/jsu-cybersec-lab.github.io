1b3d1b7949bc6109d18c52a88d4c7b35a5969e3e
Kody <kodyyu1126@outlook.com>
2026-08-23 18:34:32 +0800

    Update AMap configuration

--- assets/js/amap-config.js
+++ assets/js/amap-config.js
@@ -4,8 +4,8 @@
  * 并在高德控制台把正式网站域名加入白名单。
  */
 window.JSU_AMAP_CONFIG = Object.freeze({
-    key: 'YOUR_AMAP_WEB_KEY',
-    securityJsCode: 'YOUR_AMAP_SECURITY_JS_CODE',
+    key: '7494c806bcf864d61aa89815771113c4',
+    securityJsCode: '83e50528b87c59d8936c80cd95a5aaea',
     center: [109.725062, 28.287376],
     zoom: 17
 });
